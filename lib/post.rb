910d65d251d38c27ed1a3b63785b65af3572a9eb
munirkizilcali <munir.kizilcali@gmail.com>
2018-04-26 09:53:15 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,13 @@
+class Post
+    attr_accessor :title, :author
+
+    def initialize(name)
+        @title = name
+    end
+
+    def author_name
+        if self.author
+            self.author.name
+        end
+    end
+end
